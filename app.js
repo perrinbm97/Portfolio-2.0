7ff2effada3f1c7a5edfe2c12cd2b37b56a084cf
perrinbm97 <perrinbm08@gmail.com>
2026-03-06 01:06:43 -0500

    Added Scaling

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ function contact(event) {
         alert("Email service is temporarily unavailable. Please reach out to me directly at 'email@email.com'");
     });
 }
+
+const scaleFactor = 1 / 20;
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for(let i=0; i<shapes.length; i++) {
+    const isOdd = i % 2 !==0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+let contrastToggle = false;
+function toggleContrast() {
+  if(contrastToggle) {
+    contrastToggle = false;
+    return document.body.classList.remove("dark-mode");
+  }
+  contrastToggle = true;
+  document.body.classList += " dark-mode";
+}
+
+let isModalOpen = false;
+function toggleModal () {
+  if(isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
